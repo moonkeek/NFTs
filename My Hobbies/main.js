e8fdf9998de2bc23ba7d2c73c6e0555c84cd2348
moonkeek <107448078+moonkeek@users.noreply.github.com>
2022-06-19 18:34:39 -0700

    Add files via upload

---
+++ My Hobbies/main.js
@@ -0,0 +1,9 @@
+//var input_text;
+//var actual_password ="12345";
+//input_text=prompt('Enter the password to get to the page.');
+///if(input_text == actual_password){
+//alert('Access granted. Click Ok to view webpage.');
+//}
+//else{
+  //  window.location="https://www.google.com/";
+//}
